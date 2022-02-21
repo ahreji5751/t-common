6badd0f5cb598213b620cd89d7a682db83cbe216
Sergey Soldatov <sergey@deadlinefunnel.com>
2022-02-21 18:01:49 +0300

    Updates

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -8,6 +8,8 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
 	if (err instanceof CustomError) {
 		return res.status(err.statusCode).send({ errors: err.serializeErrors() });
 	}
+
+  console.error(err);
 	
 	res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ errors: { message: err.message } });
 }
